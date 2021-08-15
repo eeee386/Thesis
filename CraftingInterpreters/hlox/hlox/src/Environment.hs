68dbd704a8ab12214908a24746004b1b4b618ec6
Attila Sedon <attilasedon1@gmail.com>
2021-08-15 19:41:20 +0200

    Added hashtable

--- CraftingInterpreters/hlox/hlox/src/Environment.hs
+++ CraftingInterpreters/hlox/hlox/src/Environment.hs
@@ -1,4 +1,19 @@
 module Environment where
-  
---import qualified Data.HashTable.ST.Basic as HT
+
+import qualified Data.HashTable.IO as HT
+import qualified Control.Monad.ST as ST
+
+import Eval
+import AST
+
+values :: IO (HT.BasicHashTable AST.TextType EVAL)
+values = HT.new
+
+addIdentifier :: PROG_EVAL -> IO()
+addIdentifier (DEC_EVAL iden value) = do
+   val <- values
+   HT.insert val iden value 
+addIdentifier _ = return ()
+   
+
 
